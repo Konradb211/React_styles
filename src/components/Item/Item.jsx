3b2955a57b77f7cf7a9ace3329118d9efde73e5f
Konrad Baś <konradbas@MacBook-Air-Konrad.local>
2023-09-27 21:03:07 +0200

    tailwind

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,35 +1,25 @@
-import { useState } from "react"
-import styled from "styled-components"
-import { StyledButton } from "../styles/StyledButton"
+import { useState } from "react";
 
-const StyledItem = styled.li`
-	background: salmon;
-	padding: 30px 50px;
-	border-radius: 20px;
-	text-align: center;
-	overflow: hidden;
-`
+export function Item({ emoji }) {
+  const [zoomed, setZoomed] = useState(false);
 
-const StyledEmoji = styled.span`
-	display: block;
-	margin-bottom: 42px;
-	font-size: 32px;
-	transition: transform 0.3s;
-	${({ $zoomed }) => $zoomed && "transform: scale(2)"}
-`
-
-export function Item({ emoji, className }) {
-	const [zoomed, setZoomed] = useState(false)
-
-	return (
-		<StyledItem className={className}>
-			<StyledEmoji $zoomed={zoomed}>{emoji}</StyledEmoji>
-			<StyledButton
-				onClick={() => {
-					setZoomed(wasZoomed => !wasZoomed)
-				}}>
-				{zoomed ? "Oddal" : "Przybliż"}
-			</StyledButton>
-		</StyledItem>
-	)
+  return (
+    <li className="overflow-hidden rounded-[30px] bg-salmon px-12 py-8 text-center">
+      <span
+        className={`mb-10 block text-3xl transition-transform ${
+          zoomed ? "scale-[2]" : ""
+        }`}
+      >
+        {emoji}
+      </span>
+      <button
+        className={`w-[120px] cursor-pointer rounded-[5px] border border-solid border-current bg-transparent p-3 text-white transition-colors hover:bg-white hover:text-salmon`}
+        onClick={() => {
+          setZoomed((wasZoomed) => !wasZoomed);
+        }}
+      >
+        {zoomed ? "Oddal" : "Przybliż"}
+      </button>
+    </li>
+  );
 }
